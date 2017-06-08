57d966ba1931bcb5b1ebc86d650367a6c93e1b0e
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-06-08 10:47:39 +0300

    init commit

---
+++ flask_notifyAll/__init__.py
@@ -0,0 +1,5 @@
+"""
+:copyright: Mike Yusko a.k.a Frozen Monkey
+:license: MIT, see LICENSE for more details.
+"""
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+import flask_notifyAll
+
+version = flask_notifyAll.__version__
+description = 'Simple tool which will provide sms and email notifications'
+
+setup_info = {
+    'name': 'flask_notifyAll',
+    'version': version,
+    'license': 'MIT',
+    'author': 'Frozen Monkey',
+    'author_email': 'freshjelly12@yahoo.com',
+    'description': description,
+    'packages': ['flask_notifyAll'],
+    'keywords': ["flask", "sms", "notifications", "email", "send"],
+    'classifiers': [
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License'
+    ],
+}
+
+setup(**setup_info)
